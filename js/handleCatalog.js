0d9886278b8e79502621f721525472821b8186d3
moophhd <d_sizykh@mail.ru>
2020-07-18 22:21:19 +0100

    catalog: quantity selector

--- js/handleCatalog.js
+++ js/handleCatalog.js
@@ -1,5 +1,7 @@
 const getItemHtmlCatalog = (id, imgSrc, desc, price) => {
-  return `<div class="catalog-product">
+  return `<div 
+            data-quantity="1"
+            class="catalog-product">
             <img
                 class="product-img"
                 src="${imgSrc}"
@@ -9,11 +11,53 @@ const getItemHtmlCatalog = (id, imgSrc, desc, price) => {
                 ${desc}
             </p>
             <span class="product-price">${getPriceString(price)}</span>
+
+            <div class="product-selectbar">
+              <div class="selectbar-quantity">
+                <span class="quantity-title">Количество</span>
+                <div class="quantity-btn-container">
+                  <button class="quantity-btn quantity-btn-remove">−</button>
+                  <span class="quantity-count">1</span>
+                  <button class="quantity-btn quantity-btn-add">+</button>
+                </div>
+              </div>
+              <button class="selectbar-add item-add">В корзину</button>
+            </div>
         </div>`;
 };
 
+const bindCatalogSelect = () => {
+  $(".catalog-product").hover(
+    function () {
+      $(this).addClass("catalog-product--selected");
+    },
+    function () {
+      $(this).removeClass("catalog-product--selected");
+    }
+  );
+};
+
+const bindQuantityCounter = () => {
+  $(".quantity-btn").click(function () {
+    const product = $(this).parents(".catalog-product");
+    let quantity = parseInt(product.data().quantity);
+    const priceContainer = $(this).siblings(".quantity-count");
+
+    if ($(this).hasClass("quantity-btn-add")) {
+      quantity++;
+    } else {
+      quantity--
+    }
+    product.data('quantity', quantity)
+    priceContainer.text("" + quantity);
+  });
+};
+
 function handleCatalog(goods) {
+  const container = $(".catalog-products");
   Object.entries(goods).forEach(([id, data]) => {
     container.append(getItemHtmlCatalog(id, data.img, data.desc, data.price));
   });
+  bindCatalogSelect();
+  bindQuantityCounter();
 }
